8e60eb3d99c89b892abb548d153c1706dd5f9f18
mikestratton <mike@mikestratton.net>
2018-08-04 13:41:56 -0400

    resource, model, migration for prospects, notes and subscripton

--- database/migrations/2018_08_04_165341_create_prospects_table.php
+++ database/migrations/2018_08_04_165341_create_prospects_table.php
@@ -16,7 +16,18 @@ class CreateProspectsTable extends Migration
         Schema::create('prospects', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('companies_id')->index()->unsigned()->nullable();
-            //$table->integer('status_id')->index()->unsigned()->nullable();
+            $table->integer('status_id')->index()->unsigned()->nullable();
+            $table->string('name_last');
+            $table->string('name_first');
+            $table->string('email');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('city');
+            $table->string('state');
+            $table->string('county');
+            $table->integer('zip');
+            $table->string('phone');
+            $table->string('mobile');
             $table->timestamps();
 
         });
---
+++ database/migrations/2018_08_04_173322_create_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned()->nullable();
+            $table->boolean('trial')->default(0);
+            $table->boolean('paid')->default(0);
+            $table->boolean('cancelled')->default(0);
+            $table->boolean('suspended')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
