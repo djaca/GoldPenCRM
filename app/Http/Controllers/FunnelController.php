027a95f3b15a935f368b68f324f4bf1fe7a04365
mikestratton <mike@mikestratton.net>
2018-08-04 13:17:25 -0400

    migrations, models & controllers: prospect, funnel

--- app/Http/Controllers/FunnelController.php
+++ app/Http/Controllers/FunnelController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Company;
+use App\Funnel;
 use Illuminate\Http\Request;
 
-class AdminCompaniesController extends Controller
+class FunnelController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,8 +14,7 @@ class AdminCompaniesController extends Controller
      */
     public function index()
     {
-        $companies = Company::orderBy('updated_at', 'desc')->paginate(10);
-        return view('roles.admin.companies.index', compact('companies'));
+        //
     }
 
     /**
@@ -25,11 +24,7 @@ class AdminCompaniesController extends Controller
      */
     public function create()
     {
-
-        // created by admin or...
-        // happens automatically when user signs up
-        return "create something";
-
+        //
     }
 
     /**
@@ -46,10 +41,10 @@ class AdminCompaniesController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Funnel  $funnel
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Funnel $funnel)
     {
         //
     }
@@ -57,10 +52,10 @@ class AdminCompaniesController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Funnel  $funnel
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Funnel $funnel)
     {
         //
     }
@@ -69,10 +64,10 @@ class AdminCompaniesController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Funnel  $funnel
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Funnel $funnel)
     {
         //
     }
@@ -80,10 +75,10 @@ class AdminCompaniesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Funnel  $funnel
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Funnel $funnel)
     {
         //
     }
---
+++ database/migrations/2018_08_04_165341_create_prospects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProspectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prospects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('companies_id')->index()->unsigned()->nullable();
+            //$table->integer('status_id')->index()->unsigned()->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prospects');
+    }
+}
